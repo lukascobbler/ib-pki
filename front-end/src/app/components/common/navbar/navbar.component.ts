ed6c2726367aa651faf8db6e0db0e8829cff14d0
Luka <79410671+lukascobbler@users.noreply.github.com>
2025-09-15 00:14:37 +0200

    misc: initial navbar and coloring setup

--- front-end/src/app/components/common/navbar/navbar.component.ts
+++ front-end/src/app/components/common/navbar/navbar.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    RouterLink,
+    RouterLinkActive,
+    MatIconModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
