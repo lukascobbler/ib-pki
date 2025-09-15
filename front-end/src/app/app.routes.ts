48e96641e64553dcbef57a10a8839ee61d1de094
Luka <79410671+lukascobbler@users.noreply.github.com>
2025-09-15 15:53:17 +0200

    feature: adding the all certificates page for the admin user and reorganising scss styles

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {IssueCertificateComponent} from './components/common/issue-certificate/issue-certificate.component';
+import {AllCertificatesComponent} from './components/admin/all-certificates/all-certificates.component';
 
 export const routes: Routes = [
   {path: "issue-certificate", component: IssueCertificateComponent},
+  {path: "all-certificates", component: AllCertificatesComponent},
 ];
--- front-end/src/app/components/admin/all-certificates/all-certificates.component.ts
+++ front-end/src/app/components/admin/all-certificates/all-certificates.component.ts
@@ -1,12 +1,258 @@
 import { Component } from '@angular/core';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable
+} from '@angular/material/table';
+import {MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-all-certificates',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTable,
+    MatHeaderCell,
+    MatColumnDef,
+    MatHeaderCellDef,
+    MatCell,
+    MatCellDef,
+    MatIconButton,
+    MatHeaderRow,
+    MatRow,
+    MatRowDef,
+    MatHeaderRowDef
+  ],
   templateUrl: './all-certificates.component.html',
   styleUrl: './all-certificates.component.scss'
 })
 export class AllCertificatesComponent {
+  displayedColumns: string[] = [
+    'issuedBy',
+    'issuedTo',
+    'status',
+    'validFrom',
+    'validUntil',
+    'fingerprint',
+    'actions'
+  ];
 
+  certificatesDataSource: {issuedBy: string, issuedTo: string, status: string, validFrom: string, validUntil: string, fingerprint: string}[] = [
+
+    {
+      issuedBy: 'John',
+      issuedTo: 'John',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },{
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },{
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },{
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },{
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+    {
+      issuedBy: 'John Doe, IT, Example Corp, US',
+      issuedTo: 'John Doe, IT, Example Corp, US',
+      status: 'Active',
+      validFrom: '2024-01-01',
+      validUntil: '2026-01-01',
+      fingerprint: '9A:BC:DE:F1:23:45:67:89:9A:BC:DE:F1:23:45:67:89'
+    },
+  ];
 }
