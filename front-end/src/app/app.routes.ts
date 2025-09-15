42222ba9f01c2571ee6a4493271a10ac838fdf9f
Luka <79410671+lukascobbler@users.noreply.github.com>
2025-09-15 14:03:09 +0200

    feature: adding the issue certificate page

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {IssueCertificateComponent} from './components/common/issue-certificate/issue-certificate.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "issue-certificate", component: IssueCertificateComponent},
+];
--- front-end/src/app/components/common/issue-certificate/issue-certificate.component.ts
+++ front-end/src/app/components/common/issue-certificate/issue-certificate.component.ts
@@ -1,12 +1,61 @@
 import { Component } from '@angular/core';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {
+  MatDatepicker,
+  MatDatepickerInput,
+  MatDatepickerModule,
+  MatDatepickerToggle
+} from '@angular/material/datepicker';
+import {MatInput} from '@angular/material/input';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MatNativeDateModule,
+  MatOption,
+} from '@angular/material/core';
+import {MatSelect} from '@angular/material/select';
+import {MatIconButton} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {NgForOf, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {CustomDateAdapter} from '../../shared/custom-calendar/custom-date-adapter';
+import {CUSTOM_DATE_FORMATS} from '../../shared/custom-calendar/custom-date-formats';
 
 @Component({
   selector: 'app-issue-certificate',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatDatepickerInput,
+    MatDatepickerToggle,
+    MatDatepicker,
+    MatInput,
+    MatOption,
+    MatSelect,
+    MatIconButton,
+    NgForOf,
+    MatIconModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    NgIf,
+  ],
+  providers: [
+    {provide: DateAdapter, useClass: CustomDateAdapter},
+    {provide: MAT_DATE_FORMATS, useValue: CUSTOM_DATE_FORMATS}
+  ],
   templateUrl: './issue-certificate.component.html',
   styleUrl: './issue-certificate.component.scss'
 })
 export class IssueCertificateComponent {
+  extensions: {key: string, value: string, type: number}[] = [];
 
+  addExtension() {
+    this.extensions.push({ key: '', value: '', type: Math.round(Math.random()) });
+  }
+
+  removeExtension(index: number) {
+    this.extensions.splice(index, 1);
+  }
 }
