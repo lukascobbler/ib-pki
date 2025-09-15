d393cb3b826b2612dd0c3158dd8687e702e763f4
Luka <79410671+lukascobbler@users.noreply.github.com>
2025-09-15 17:14:34 +0200

    feature: adding the login page

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import {IssueCertificateComponent} from './components/common/issue-certificate/issue-certificate.component';
 import {AllCertificatesComponent} from './components/admin/all-certificates/all-certificates.component';
 import {CaUserManagementComponent} from './components/admin/ca-user-management/ca-user-management.component';
+import {LoginComponent} from './components/anonymous/login/login.component';
 
 export const routes: Routes = [
+  {path: "login", component: LoginComponent},
   {path: "issue-certificate", component: IssueCertificateComponent},
   {path: "all-certificates", component: AllCertificatesComponent},
   {path: "manage-ca-users", component: CaUserManagementComponent},
--- front-end/src/app/components/anonymous/login/login.component.ts
+++ front-end/src/app/components/anonymous/login/login.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    MatFormField,
+    MatInput,
+    MatLabel
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
